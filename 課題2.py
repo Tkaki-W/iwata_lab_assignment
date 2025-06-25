9d5701c03a437d4f6c9e0ecf97179e2cd6ca95ef
Tkaki-W <takanoda2005@gmail.com>
2025-06-25 19:19:00 +0900

    functionized

--- 課題2.py
+++ 課題2.py
@@ -1,13 +1,17 @@
 import math
 import random
 
+def check(input, correct):
+    if input == correct:
+        print("正解!")
+    else:
+        print("不正解")
+
 correct = 100
 #random.randint(100, 999)
 
 '''int型にしないと判定できない'''
 input = int(input("３桁の文字列を入力してください: "))
 print(input)
-if input == correct:
-    print("正解!")
-else:
-    print("不正解")
+check(input, correct)
+
