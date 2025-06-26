3708ca78af4a842763d3a6a465e9f4ce844579da
Tkaki-W <takanoda2005@gmail.com>
2025-06-26 10:57:50 +0900

    roop_added

--- hint.py
+++ hint.py
@@ -8,13 +8,18 @@ class Hint:
         i = 0
         print(self.input)
         for i in range(19):
-            print(i)
             min = 100+i*50
             max = 150+i*50-1
             if min <= self.correct <= max:
                 print(f"求める数は{min}から{max}の中です")
+                break
         
-
+    def multiple(self):
+        n = 2
+        for n in range(32):
+            if self.correct % n == 0:
+                break
+        print(f"求める数は{n}の倍数です")
 
 
             
--- 課題2.py
+++ 課題2.py
@@ -2,26 +2,32 @@ import math
 import random
 from hint import Hint 
 
-def hint(input, correct, count):
+    
+def hint_output(answer, correct, count):
     "ヒントが呼び出されるとインスタンス生成"
-    instance1 = Hint(input, correct)
+    instance1 = Hint(answer, correct)
     if count == 1:
         instance1.range()
+    elif count == 2:
+        instance1.multiple()
 
-def check(A, B, count):
+def check(A, B):
+    global count
     if A == B:
         print("正解!")
     else:
         print("不正解")
-        print(count)
         count+=1
-        hint(input, correct, count)
+        print(count)
+        hint_output(answer, correct, count)
 
 count = 0
 correct = 200
 #random.randint(100, 999)
 
-'''int型にしないと判定できない'''
-input = int(input("３桁の文字列を入力してください: "))
-print(input)
-check(input, correct, count)
+while True:
+    print(count)
+    '''int型にしないと判定できない'''
+    answer = int(input("３桁の文字列を入力してください: "))
+    print(answer)
+    check(answer, correct)
